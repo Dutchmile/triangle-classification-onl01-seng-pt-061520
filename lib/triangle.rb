8806a444ad1ef13909b793631fcf17373ab305d0
Christopher Dutcher <cadutcher@icloud.com>
2020-08-10 00:35:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,9 @@ class Triangle
   end
 
   def kind
-    if @side2 == @side3 && @side1 != @side2
+    if @side2 == @side3 && @side1 != @side2 
+      self.type = :isosceles
+    elsif @side1 == @side3 && @side1 != @side 2
       self.type = :isosceles
     elsif @side1 == @side2 && @side1 == @side3
       self.type = :equilateral
