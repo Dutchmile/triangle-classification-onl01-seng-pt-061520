936a4d837edd39874c48efce6550e3ef9230f81b
Christopher Dutcher <cadutcher@icloud.com>
2020-08-10 00:30:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+
+  attr_reader :side1_length, :side2_length, :side3_length
+
+  def initialize(side1_length,side2_length,side3_length)
+    @side1 = side1_length
+    @side2 = side2_length
+    @side3 = side3_length
+  end
+
+  def kind
+    if @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+      self.type = "isosceles"
+    elsif @side1 == @side2 && @side1 == @side3
+      self.type = "equilateral"
+    else
+      raise TriangleError
+    end
+  end
+  
 end
